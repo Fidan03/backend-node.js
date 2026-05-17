5e999b92b2616073896b2dec6f299befea56ca65
Fidan <xelilovafidan61@gmail.com>
2026-05-17 13:31:54 +0400

    fix: correct userId reference in watchlistController and enhance authMiddleware for token validation

--- src/controllers/watchlistController.js
+++ src/controllers/watchlistController.js
@@ -16,7 +16,7 @@ const addToWatchlist = async (req, res) => {
     const existingInWatchlist = await prisma.watchlistItem.findUnique({
         where: {
             userId_movieId: {
-                userId: userId,
+                userId: req.user.id,
                 movieId: movieId,
             },
         },
@@ -28,7 +28,7 @@ const addToWatchlist = async (req, res) => {
 
     const watchlistItem = await prisma.watchlistItem.create({
         data: {
-            userId,
+            userId: req.user.id,
             movieId,
             status: status || "PLANNED",
             rating,
