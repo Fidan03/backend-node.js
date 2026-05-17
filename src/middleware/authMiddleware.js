c389c7ade8831f18cbe7e35c91e35dbca82aa924
Fidan <xelilovafidan61@gmail.com>
2026-05-17 13:50:04 +0400

    fix: improve token retrieval logic in authMiddleware for cookie handling

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -10,7 +10,7 @@ export const authMiddleware = async (req, res, next) => {
 
     if(req.headers.authorization && req.headers.authorization.startsWith("Bearer")) {
         token = req.headers.authorization.split(" ")[1];
-    } else if(req.cookies.jwt) {
+    } else if(req.cookies?.jwt) {
         token = req.cookies.jwt
     }
 
@@ -37,3 +37,7 @@ export const authMiddleware = async (req, res, next) => {
         return res.status(401).json({error: "Not authorized, token failed"});
     }
 }
+
+
+
+    // "userId": "56f632cc-3201-435a-8084-7e7ef716d68a",
